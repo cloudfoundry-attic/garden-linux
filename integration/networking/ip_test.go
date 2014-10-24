d71c2c76cd54441010ca76a7d916e7d78394fa8a
Steve Powell <spowell@pivotal.io>
2014-10-24 11:57:20 +0100

    Refactor If name creation for tests
    [#80737264]

--- integration/networking/ip_test.go
+++ integration/networking/ip_test.go
@@ -1,13 +1,13 @@
 package networking_test
 
 import (
-	"os/exec"
-	"strconv"
-
 	"github.com/cloudfoundry-incubator/garden/api"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
+
+	"fmt"
+	"os/exec"
 )
 
 var _ = Describe("IP settings", func() {
@@ -32,16 +32,15 @@ var _ = Describe("IP settings", func() {
 			stdout := gbytes.NewBuffer()
 			stderr := gbytes.NewBuffer()
 
-			containerInterface := "w" + strconv.Itoa(GinkgoParallelNode()) + container.Handle() + "-1"
-
 			process, err := container.Run(api.ProcessSpec{
 				Path: "/sbin/ifconfig",
-				Args: []string{containerInterface},
+				Args: []string{containerIfName(container)},
 			}, api.ProcessIO{
 				Stdout: stdout,
 				Stderr: stderr,
 			})
 			Ω(err).ShouldNot(HaveOccurred())
+
 			rc, err := process.Wait()
 			Ω(err).ShouldNot(HaveOccurred())
 			Ω(rc).Should(Equal(0))
@@ -52,12 +51,26 @@ var _ = Describe("IP settings", func() {
 
 	Describe("hosts's network interface for a container", func() {
 		It("has the correct IP address", func() {
-			hostInterface := "w" + strconv.Itoa(GinkgoParallelNode()) + container.Handle() + "-0"
-
-			out, err := exec.Command("/sbin/ifconfig", hostInterface).Output()
+			out, err := exec.Command("/sbin/ifconfig", hostIfName(container)).Output()
 			Ω(err).ShouldNot(HaveOccurred())
 
 			Ω(out).Should(ContainSubstring(" inet addr:10.3.0.254 "))
+		})
+	})
+
+	Describe("the container's network", func() {
+
+		It("is reachable from host", func() {
+			info, ierr := container.Info()
+			Ω(ierr).ShouldNot(HaveOccurred())
+
+			cmd := exec.Command("/bin/ping", "-c 2 -o -t 2 "+info.HostIP)
+			fmt.Println(cmd)
+
+			out, err := cmd.Output()
+			Ω(err).ShouldNot(HaveOccurred())
+
+			Ω(out).Should(Equal(""))
 		})
 	})
 
