b2007cd6ba72f74a84e51ea896fe921a65b7a537
Julz Friedman <julz.friedman@uk.ibm.com>
2014-11-20 13:58:33 +0000

    Fix up tests to work with user namespacing.
    [#82838560]

--- integration/lifecycle/lifecycle_test.go
+++ integration/lifecycle/lifecycle_test.go
@@ -67,7 +67,7 @@ var _ = Describe("Creating a container", func() {
 
 		Ω(process.Wait()).Should(Equal(0))
 
-		Ω(outBuf).Should(gbytes.Say("tmpfs /dev/shm tmpfs rw,relatime 0 0"))
+		Ω(outBuf).Should(gbytes.Say("tmpfs /dev/shm tmpfs rw,nodev,relatime 0 0"))
 	})
 
 	Context("and sending a List request", func() {
