a3e33e58d8fe0c9127a6f28c839b5ef8ad305562
Julz Friedman <julz.friedman@uk.ibm.com>
2014-11-21 10:31:23 +0000

    Do mounts and networking config in host.
    Once the root user is not always container root, it may not have
    permissions to execute mounts and networking operations inside the
    container. Therefore we have to do these operations in the host.
    
    Also explicitly setuid and setgid to 0 so that we become container root
    before running hook scripts.
    
    We no longer have permissions to delete tmp/garden-host by the time the
    container is created, so for now we're going to have to leave it.
    See #83193088 which tracks figuring out how to delete this.
    
    [#82838560]

--- old/linux_backend/skeleton/lib/hook-child-after-pivot.sh
+++ old/linux_backend/skeleton/lib/hook-child-after-pivot.sh
@@ -8,25 +8,7 @@ cd $(dirname $0)/../
 
 . etc/config
 
-mkdir -p /dev/pts
-mount -t devpts -o newinstance,ptmxmode=0666 devpts /dev/pts
-ln -sf pts/ptmx /dev/ptmx
-
-mkdir -p /proc
-mount -t proc none /proc
-
-mkdir -p /dev/shm
-mount -t tmpfs tmpfs /dev/shm
-
 hostname $id
-
-ip address add 127.0.0.1/8 dev lo
-ip link set lo up
-
-ip address add $network_container_ip/$network_cidr_suffix dev $network_container_iface
-ip link set $network_container_iface mtu $container_iface_mtu up
-
-ip route add default via $network_host_ip dev $network_container_iface
 
 if [ -e /etc/seed ]; then
   . /etc/seed
