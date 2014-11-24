2d5ed31e636dfa7d092c99fbf6df6b7c79d4966f
Julz Friedman <julz.friedman@uk.ibm.com>
2014-11-24 09:40:48 +0000

    Remove 64k limit on /dev/shm
    Is included in memory limit anyway, no need for a seperate limit. This
    was inadvertently reintroduced by the user namespacing changes.
    
    [#82838560]

--- old/linux_backend/skeleton/lib/hook-parent-before-clone.sh
+++ old/linux_backend/skeleton/lib/hook-parent-before-clone.sh
@@ -18,4 +18,4 @@ mount -n -t devpts -o newinstance,ptmxmode=0666 devpts $rootfs_path/dev/pts
 mkdir -p $rootfs_path/proc
 mount -n -t proc none $rootfs_path/proc
 mkdir -p $rootfs_path/dev/shm
-mount -n -t tmpfs -o nodev,size=64k tmpfs $rootfs_path/dev/shm
+mount -n -t tmpfs -o nodev tmpfs $rootfs_path/dev/shm
