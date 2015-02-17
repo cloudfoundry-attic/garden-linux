3efe2ed497ffc3f14d6ca42c47bca3508aa9b0d2
Tom Brand <tbrand@pivotal.io>
2015-02-17 12:21:53 +0000

    Add basic unit test for iodaemon
    We can now run iodaemon in the same process as the test runner allowing
    coverage reports to be produced.
    
    [#88326712]
    
    Signed-off-by: Glyn Normington <gnormington@pivotal.io>

--- old/iodaemon/iodaemon_test.go
+++ old/iodaemon/iodaemon_test.go
@@ -1,70 +1,45 @@
-package main_test
+package main
 
 import (
+	"time"
+
+	"io/ioutil"
 	"os"
-	"os/exec"
+	"path/filepath"
 
 	linkpkg "github.com/cloudfoundry-incubator/garden-linux/old/iodaemon/link"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
-	"github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("Iodaemon", func() {
-	It("can read stdin", func() {
-		spawnS, err := gexec.Start(exec.Command(
-			iodaemon,
-			"spawn",
-			socketPath,
-			"bash", "-c", "cat <&0; exit 42",
-		), GinkgoWriter, GinkgoWriter)
+	var (
+		socketPath string
+		tmpdir     string
+	)
+	BeforeEach(func() {
+		var err error
+		tmpdir, err = ioutil.TempDir("", "socket-dir")
 		Ω(err).ShouldNot(HaveOccurred())
 
-		defer spawnS.Kill()
+		socketPath = filepath.Join(tmpdir, "iodaemon.sock")
+	})
 
-		Eventually(spawnS).Should(gbytes.Say("ready\n"))
-		Consistently(spawnS).ShouldNot(gbytes.Say("pid:"))
+	AfterEach(func() {
+		os.RemoveAll(tmpdir)
+	})
+
+	It("spawns", func() {
+		args := []string{"echo", "hello"}
+		go spawn(socketPath, args, time.Second, false, 0, 0, false)
 
 		linkStdout := gbytes.NewBuffer()
-		link, err := linkpkg.Create(socketPath, linkStdout, os.Stderr)
-		Ω(err).ShouldNot(HaveOccurred())
-
-		link.Write([]byte("hello\ngoodbye"))
-		link.Close()
-
-		Eventually(spawnS).Should(gbytes.Say("pid:"))
-		Eventually(linkStdout).Should(gbytes.Say("hello\ngoodbye"))
-
-		Ω(link.Wait()).Should(Equal(42))
+		Eventually(func() error {
+			_, err := linkpkg.Create(socketPath, linkStdout, os.Stderr)
+			return err
+		}, time.Second).ShouldNot(HaveOccurred())
+		Eventually(linkStdout).Should(gbytes.Say("hello\n"))
 	})
 
-	It("consistently executes a quickly-printing-and-exiting command", func() {
-		for i := 0; i < 100; i++ {
-			spawnS, err := gexec.Start(exec.Command(
-				iodaemon,
-				"spawn",
-				socketPath,
-				"echo", "hi",
-			), GinkgoWriter, GinkgoWriter)
-			Ω(err).ShouldNot(HaveOccurred())
-
-			Eventually(spawnS).Should(gbytes.Say("ready\n"))
-
-			link := exec.Command(iodaemon, "link", socketPath)
-
-			linkS, err := gexec.Start(link, GinkgoWriter, GinkgoWriter)
-			Ω(err).ShouldNot(HaveOccurred())
-
-			Eventually(spawnS).Should(gbytes.Say("pid:"))
-
-			Eventually(linkS).Should(gbytes.Say("hi"))
-			Eventually(linkS).Should(gexec.Exit(0))
-
-			Eventually(spawnS).Should(gexec.Exit(0))
-
-			err = os.Remove(socketPath)
-			Ω(err).ShouldNot(HaveOccurred())
-		}
-	})
 })
