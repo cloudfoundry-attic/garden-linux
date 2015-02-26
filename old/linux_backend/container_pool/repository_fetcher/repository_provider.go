1ea17005912a84f6f775e043106b4cdce435c29f
Tom Brand <tbrand@pivotal.io>
2015-02-26 17:51:28 +0000

    Add more specific message when failing to connect to insecure registry
    [finishes #88888066]
    
    Signed-off-by: Julian Friedman <julz.friedman@uk.ibm.com>

--- old/linux_backend/container_pool/repository_fetcher/repository_provider.go
+++ old/linux_backend/container_pool/repository_fetcher/repository_provider.go
@@ -1,8 +1,20 @@
 package repository_fetcher
 
+import "strings"
+
 //go:generate counterfeiter . RegistryProvider
 type RegistryProvider interface {
 	ProvideRegistry(hostname string) (Registry, error)
+}
+
+type InsecureRegistryError struct {
+	Cause              error
+	Endpoint           string
+	InsecureRegistries []string
+}
+
+func (InsecureRegistryError) Error() string {
+	return "repository_provider: Unable to fetch RootFS image from insecure docker registry URL.  To enable insecure access from this host, add it to the -insecureDockerRegistryList on boot."
 }
 
 type registryProvider struct {
@@ -18,7 +30,13 @@ func (rp registryProvider) ProvideRegistry(hostname string) (Registry, error) {
 	}
 
 	endpoint, err := RegistryNewEndpoint(hostname, rp.InsecureRegistries)
-	if err != nil {
+	if err != nil && strings.Contains(err.Error(), "--insecure-registry") {
+		return nil, &InsecureRegistryError{
+			Cause:              err,
+			Endpoint:           hostname,
+			InsecureRegistries: rp.InsecureRegistries,
+		}
+	} else if err != nil {
 		return nil, err
 	}
 
