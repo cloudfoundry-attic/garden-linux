6f0930a05989cd991bea473e9136ce8feba5cdd6
Glyn Normington <gnormington@pivotal.io>
2015-03-18 10:31:08 +0000

    Split metrics out of Linux container.
    [#89713180]

---
+++ linux_container/metrics.go
@@ -0,0 +1,154 @@
+package linux_container
+
+import (
+	"bufio"
+	"strconv"
+	"strings"
+
+	"github.com/cloudfoundry-incubator/garden"
+)
+
+func (c *LinuxContainer) Metrics() (garden.Metrics, error) {
+	cLog := c.logger.Session("metrics")
+
+	diskStat, err := c.quotaManager.GetUsage(cLog, c.resources.UserUID)
+	if err != nil {
+		return garden.Metrics{}, err
+	}
+
+	cpuStat, err := c.cgroupsManager.Get("cpuacct", "cpuacct.stat")
+	if err != nil {
+		return garden.Metrics{}, err
+	}
+
+	cpuUsage, err := c.cgroupsManager.Get("cpuacct", "cpuacct.usage")
+	if err != nil {
+		return garden.Metrics{}, err
+	}
+
+	memoryStat, err := c.cgroupsManager.Get("memory", "memory.stat")
+	if err != nil {
+		return garden.Metrics{}, err
+	}
+
+	return garden.Metrics{
+		MemoryStat: parseMemoryStat(memoryStat),
+		CPUStat:    parseCPUStat(cpuUsage, cpuStat),
+		DiskStat:   diskStat,
+	}, nil
+}
+
+func parseMemoryStat(contents string) (stat garden.ContainerMemoryStat) {
+	scanner := bufio.NewScanner(strings.NewReader(contents))
+
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		field := scanner.Text()
+
+		if !scanner.Scan() {
+			break
+		}
+
+		value, err := strconv.ParseUint(scanner.Text(), 10, 0)
+		if err != nil {
+			continue
+		}
+
+		switch field {
+		case "cache":
+			stat.Cache = value
+		case "rss":
+			stat.Rss = value
+		case "mapped_file":
+			stat.MappedFile = value
+		case "pgpgin":
+			stat.Pgpgin = value
+		case "pgpgout":
+			stat.Pgpgout = value
+		case "swap":
+			stat.Swap = value
+		case "pgfault":
+			stat.Pgfault = value
+		case "pgmajfault":
+			stat.Pgmajfault = value
+		case "inactive_anon":
+			stat.InactiveAnon = value
+		case "active_anon":
+			stat.ActiveAnon = value
+		case "inactive_file":
+			stat.InactiveFile = value
+		case "active_file":
+			stat.ActiveFile = value
+		case "unevictable":
+			stat.Unevictable = value
+		case "hierarchical_memory_limit":
+			stat.HierarchicalMemoryLimit = value
+		case "hierarchical_memsw_limit":
+			stat.HierarchicalMemswLimit = value
+		case "total_cache":
+			stat.TotalCache = value
+		case "total_rss":
+			stat.TotalRss = value
+		case "total_mapped_file":
+			stat.TotalMappedFile = value
+		case "total_pgpgin":
+			stat.TotalPgpgin = value
+		case "total_pgpgout":
+			stat.TotalPgpgout = value
+		case "total_swap":
+			stat.TotalSwap = value
+		case "total_pgfault":
+			stat.TotalPgfault = value
+		case "total_pgmajfault":
+			stat.TotalPgmajfault = value
+		case "total_inactive_anon":
+			stat.TotalInactiveAnon = value
+		case "total_active_anon":
+			stat.TotalActiveAnon = value
+		case "total_inactive_file":
+			stat.TotalInactiveFile = value
+		case "total_active_file":
+			stat.TotalActiveFile = value
+		case "total_unevictable":
+			stat.TotalUnevictable = value
+		}
+	}
+
+	return
+}
+
+func parseCPUStat(usage, statContents string) (stat garden.ContainerCPUStat) {
+	cpuUsage, err := strconv.ParseUint(strings.Trim(usage, "\n"), 10, 0)
+	if err != nil {
+		return
+	}
+
+	stat.Usage = cpuUsage
+
+	scanner := bufio.NewScanner(strings.NewReader(statContents))
+
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		field := scanner.Text()
+
+		if !scanner.Scan() {
+			break
+		}
+
+		value, err := strconv.ParseUint(scanner.Text(), 10, 0)
+		if err != nil {
+			continue
+		}
+
+		switch field {
+		case "user":
+			stat.User = value
+		case "system":
+			stat.System = value
+		}
+	}
+
+	return
+}
---
+++ linux_container/metrics_test.go
@@ -0,0 +1,240 @@
+package linux_container_test
+
+import (
+	"errors"
+	"net"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/pivotal-golang/lager/lagertest"
+
+	"github.com/cloudfoundry-incubator/garden"
+	"github.com/cloudfoundry-incubator/garden-linux/linux_backend"
+	"github.com/cloudfoundry-incubator/garden-linux/linux_container"
+	networkFakes "github.com/cloudfoundry-incubator/garden-linux/network/fakes"
+	"github.com/cloudfoundry-incubator/garden-linux/old/bandwidth_manager/fake_bandwidth_manager"
+	"github.com/cloudfoundry-incubator/garden-linux/old/cgroups_manager/fake_cgroups_manager"
+	"github.com/cloudfoundry-incubator/garden-linux/old/port_pool/fake_port_pool"
+	"github.com/cloudfoundry-incubator/garden-linux/old/quota_manager/fake_quota_manager"
+	"github.com/cloudfoundry-incubator/garden-linux/process"
+	"github.com/cloudfoundry-incubator/garden-linux/process_tracker/fake_process_tracker"
+	"github.com/cloudfoundry/gunk/command_runner/fake_command_runner"
+)
+
+var _ = Describe("Linux containers", func() {
+
+	BeforeEach(func() {
+		fakeCgroups = fake_cgroups_manager.New("/cgroups", "some-id")
+
+		fakeQuotaManager = fake_quota_manager.New()
+	})
+
+	JustBeforeEach(func() {
+		_, subnet, _ := net.ParseCIDR("2.3.4.0/30")
+		containerResources := linux_backend.NewResources(
+			1234,
+			1235,
+			&linux_backend.Network{
+				IP:     net.ParseIP("1.2.3.4"),
+				Subnet: subnet,
+			},
+			"some-bridge",
+			[]uint32{},
+			nil,
+		)
+		container = linux_container.NewLinuxContainer(
+			lagertest.NewTestLogger("test"),
+			"some-id",
+			"some-handle",
+			containerDir,
+			nil,
+			1*time.Second,
+			containerResources,
+			fake_port_pool.New(1000),
+			fake_command_runner.New(),
+			fakeCgroups,
+			fakeQuotaManager,
+			fake_bandwidth_manager.New(),
+			new(fake_process_tracker.FakeProcessTracker),
+			process.Env{"env1": "env1Value", "env2": "env2Value"},
+			new(networkFakes.FakeFilter),
+		)
+	})
+
+	Describe("Metrics", func() {
+		Describe("memory info", func() {
+			BeforeEach(func() {
+				fakeCgroups.WhenGetting("memory", "memory.stat", func() (string, error) {
+					return `cache 1
+rss 2
+mapped_file 3
+pgpgin 4
+pgpgout 5
+swap 6
+pgfault 7
+pgmajfault 8
+inactive_anon 9
+active_anon 10
+inactive_file 11
+active_file 12
+unevictable 13
+hierarchical_memory_limit 14
+hierarchical_memsw_limit 15
+total_cache 16
+total_rss 17
+total_mapped_file 18
+total_pgpgin 19
+total_pgpgout 20
+total_swap 21
+total_pgfault 22
+total_pgmajfault 23
+total_inactive_anon 24
+total_active_anon 25
+total_inactive_file 26
+total_active_file 27
+total_unevictable 28
+`, nil
+				})
+			})
+
+			It("is returned in the response", func() {
+				metrics, err := container.Metrics()
+				Ω(err).ShouldNot(HaveOccurred())
+				Ω(metrics.MemoryStat).Should(Equal(garden.ContainerMemoryStat{
+					Cache:                   1,
+					Rss:                     2,
+					MappedFile:              3,
+					Pgpgin:                  4,
+					Pgpgout:                 5,
+					Swap:                    6,
+					Pgfault:                 7,
+					Pgmajfault:              8,
+					InactiveAnon:            9,
+					ActiveAnon:              10,
+					InactiveFile:            11,
+					ActiveFile:              12,
+					Unevictable:             13,
+					HierarchicalMemoryLimit: 14,
+					HierarchicalMemswLimit:  15,
+					TotalCache:              16,
+					TotalRss:                17,
+					TotalMappedFile:         18,
+					TotalPgpgin:             19,
+					TotalPgpgout:            20,
+					TotalSwap:               21,
+					TotalPgfault:            22,
+					TotalPgmajfault:         23,
+					TotalInactiveAnon:       24,
+					TotalActiveAnon:         25,
+					TotalInactiveFile:       26,
+					TotalActiveFile:         27,
+					TotalUnevictable:        28,
+				}))
+
+			})
+		})
+
+		Context("when getting memory.stat fails", func() {
+			disaster := errors.New("oh no!")
+
+			JustBeforeEach(func() {
+				fakeCgroups.WhenGetting("memory", "memory.stat", func() (string, error) {
+					return "", disaster
+				})
+			})
+
+			It("returns an error", func() {
+				_, err := container.Metrics()
+				Ω(err).Should(Equal(disaster))
+			})
+		})
+
+		Describe("cpu info", func() {
+			BeforeEach(func() {
+				fakeCgroups.WhenGetting("cpuacct", "cpuacct.usage", func() (string, error) {
+					return `42
+`, nil
+				})
+
+				fakeCgroups.WhenGetting("cpuacct", "cpuacct.stat", func() (string, error) {
+					return `user 1
+system 2
+`, nil
+				})
+			})
+
+			It("is returned in the response", func() {
+				metrics, err := container.Metrics()
+				Ω(err).ShouldNot(HaveOccurred())
+				Ω(metrics.CPUStat).Should(Equal(garden.ContainerCPUStat{
+					Usage:  42,
+					User:   1,
+					System: 2,
+				}))
+
+			})
+		})
+
+		Context("when getting cpuacct/cpuacct.usage fails", func() {
+			disaster := errors.New("oh no!")
+
+			JustBeforeEach(func() {
+				fakeCgroups.WhenGetting("cpuacct", "cpuacct.usage", func() (string, error) {
+					return "", disaster
+				})
+			})
+
+			It("returns an error", func() {
+				_, err := container.Metrics()
+				Ω(err).Should(Equal(disaster))
+			})
+		})
+
+		Context("when getting cpuacct/cpuacct.stat fails", func() {
+			disaster := errors.New("oh no!")
+
+			JustBeforeEach(func() {
+				fakeCgroups.WhenGetting("cpuacct", "cpuacct.stat", func() (string, error) {
+					return "", disaster
+				})
+			})
+
+			It("returns an error", func() {
+				_, err := container.Metrics()
+				Ω(err).Should(Equal(disaster))
+			})
+		})
+
+		Describe("disk usage info", func() {
+			It("is returned in the response", func() {
+				fakeQuotaManager.GetUsageResult = garden.ContainerDiskStat{
+					BytesUsed:  1,
+					InodesUsed: 2,
+				}
+
+				metrics, err := container.Metrics()
+				Ω(err).ShouldNot(HaveOccurred())
+
+				Ω(metrics.DiskStat).Should(Equal(garden.ContainerDiskStat{
+					BytesUsed:  1,
+					InodesUsed: 2,
+				}))
+
+			})
+
+			Context("when getting the disk usage fails", func() {
+				disaster := errors.New("oh no!")
+
+				JustBeforeEach(func() {
+					fakeQuotaManager.GetUsageError = disaster
+				})
+
+				It("returns the error", func() {
+					_, err := container.Metrics()
+					Ω(err).Should(Equal(disaster))
+				})
+			})
+		})
+	})
+})
