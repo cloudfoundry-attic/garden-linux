23b30704a7ee02e2af05f54bacdcce5e4c1dbebb
George Lestaris <glestaris@pivotal.io>
2015-03-20 16:59:26 +0000

    Add manual performance test.
    Note that the test needs to be skipped in such a way that RunSpecs is called
    to avoid spurious output from ginkgo.
    
    [finishes #89018332]
    
    Signed-off-by: Glyn Normington <gnormington@pivotal.io>

---
+++ integration/performance/README.md
@@ -0,0 +1,5 @@
+The tests in this directory are for manual performance testing and not part of CI.
+
+A local garden-linux must be running on port 7777.
+
+$GARDEN_PERFORMANCE must be set to enable this test.
---
+++ integration/performance/create_measure_test.go
@@ -0,0 +1,67 @@
+package performance_test
+
+import (
+	"runtime"
+	"strconv"
+
+	"log"
+	"os"
+
+	"github.com/cloudfoundry-incubator/garden"
+	gclient "github.com/cloudfoundry-incubator/garden/client"
+	"github.com/cloudfoundry-incubator/garden/client/connection"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+const (
+	creates       = 40
+	createSamples = 5
+)
+
+var _ = Describe("Concurrent container creation", func() {
+
+	BeforeEach(func() {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+	})
+
+	Measure("multiple concurrent creates", func(b Benchmarker) {
+		if os.Getenv("GARDEN_PERFORMANCE") == "" {
+			log.Println("GARDEN_PERFORMANCE undefined; skipping")
+			return
+		}
+		handles := []string{}
+
+		b.Time("concurrent creations", func() {
+			chans := []chan string{}
+			for i := 0; i < creates; i++ {
+				ch := make(chan string, 1)
+				go func(c chan string, index int) {
+					defer GinkgoRecover()
+					client := gclient.New(connection.New("tcp", "localhost:7777"))
+					b.Time("create-"+strconv.Itoa(index), func() {
+						ctr, err := client.Create(garden.ContainerSpec{})
+						Ω(err).ShouldNot(HaveOccurred())
+						c <- ctr.Handle()
+					})
+				}(ch, i)
+				chans = append(chans, ch)
+			}
+
+			for _, ch := range chans {
+				handle := <-ch
+				if handle != "" {
+					handles = append(handles, handle)
+
+				}
+			}
+		})
+
+		for _, handle := range handles {
+			client := gclient.New(connection.New("tcp", "localhost:7777"))
+			Ω(client.Destroy(handle)).Should(Succeed())
+		}
+
+	}, createSamples)
+
+})
