7392329a4fe61e9a16e794ae2313f79d9efbd076
Craig Furman <cfurman@pivotal.io>
2015-04-01 16:25:11 +0100

    Configure container's network in hook executable.
    Signed-off-by: Glyn Normington <gnormington@pivotal.io>

--- linux_backend/hooks.go
+++ linux_backend/hooks.go
@@ -2,10 +2,15 @@ package linux_backend
 
 import (
 	"encoding/json"
+	"net"
 	"os"
 	"os/exec"
 
+	"strconv"
+
+	"github.com/cloudfoundry-incubator/cf-lager"
 	"github.com/cloudfoundry-incubator/garden-linux/hook"
+	"github.com/cloudfoundry-incubator/garden-linux/network"
 	"github.com/cloudfoundry-incubator/garden-linux/process"
 )
 
@@ -23,6 +28,22 @@ func RegisterHooks(hs hook.HookSet, runner Runner, config process.Env, container
 	})
 
 	hs.Register(hook.CHILD_AFTER_PIVOT, func() {
+		_, ipNet, err := net.ParseCIDR(config["network_cidr"])
+		must(err)
+
+		mtu, err := strconv.ParseInt(config["container_iface_mtu"], 0, 64)
+		must(err)
+
+		logger := cf_lager.New("linux_backend: hook.CHILD_AFTER_PIVOT")
+		c := network.NewConfigurer(logger)
+
+		must(c.ConfigureContainer(config["network_container_iface"],
+			net.ParseIP(config["network_container_ip"]),
+			net.ParseIP(config["network_host_ip"]),
+			ipNet,
+			int(mtu),
+		))
+
 		must(container.SetHostname(config["id"]))
 		must(container.MountProc())
 		must(container.MountTmp())
