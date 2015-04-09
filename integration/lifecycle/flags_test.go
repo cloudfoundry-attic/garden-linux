2bec1d031420d9d6eef44dceca19e717b2471d33
Craig Furman <cfurman@pivotal.io>
2015-04-09 13:48:12 +0100

    Refactor flags_test
    Signed-off-by: Alex Ley <aley@pivotal.io>

--- integration/lifecycle/flags_test.go
+++ integration/lifecycle/flags_test.go
@@ -8,40 +8,52 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Garden", func() {
+var _ = Describe("Garden startup flags", func() {
 
 	var debugAddr string
 
 	BeforeEach(func() {
 		debugAddr = fmt.Sprintf("0.0.0.0:%d", 15000+GinkgoParallelNode())
-
 	})
 
-	It("started without --debugAddr flag", func() {
-		client = startGarden()
+	Context("when starting without the --debugAddr flag", func() {
+		BeforeEach(func() {
+			client = startGarden()
+		})
 
-		_, err := http.Get(fmt.Sprintf("http://%s/debug/pprof/?debug=1", debugAddr))
-		Ω(err).Should(HaveOccurred())
+		It("does not expose the pprof debug endpoint", func() {
+			_, err := http.Get(fmt.Sprintf("http://%s/debug/pprof/?debug=1", debugAddr))
+			Ω(err).Should(HaveOccurred())
+		})
 
+		It("does not expose the log level adjustment endpoint", func() {
+			_, err := http.Get(fmt.Sprintf("http://%s/log-level -X PUT -d debug", debugAddr))
+			Ω(err).Should(HaveOccurred())
+		})
 	})
 
-	It("started with --debugAddr flag", func() {
-		client = startGarden("--debugAddr", debugAddr)
+	Context("when starting with the --debugAddr flag", func() {
+		BeforeEach(func() {
+			client = startGarden("--debugAddr", debugAddr)
+		})
 
-		_, err := http.Get(fmt.Sprintf("http://%s/debug/pprof/?debug=1", debugAddr))
-		Ω(err).ShouldNot(HaveOccurred())
+		It("exposes the pprof debug endpoint", func() {
+			_, err := http.Get(fmt.Sprintf("http://%s/debug/pprof/?debug=1", debugAddr))
+			Ω(err).ShouldNot(HaveOccurred())
+		})
 
-		_, err = http.Get(fmt.Sprintf("http://%s/log-level -X PUT -d debug", debugAddr))
-		Ω(err).ShouldNot(HaveOccurred())
+		It("exposes the log level adjustment endpoint", func() {
+			_, err := http.Get(fmt.Sprintf("http://%s/log-level -X PUT -d debug", debugAddr))
+			Ω(err).ShouldNot(HaveOccurred())
 
-		_, err = http.Get(fmt.Sprintf("http://%s/log-level -X PUT -d info", debugAddr))
-		Ω(err).ShouldNot(HaveOccurred())
+			_, err = http.Get(fmt.Sprintf("http://%s/log-level -X PUT -d info", debugAddr))
+			Ω(err).ShouldNot(HaveOccurred())
 
-		_, err = http.Get(fmt.Sprintf("http://%s/log-level -X PUT -d error", debugAddr))
-		Ω(err).ShouldNot(HaveOccurred())
+			_, err = http.Get(fmt.Sprintf("http://%s/log-level -X PUT -d error", debugAddr))
+			Ω(err).ShouldNot(HaveOccurred())
 
-		_, err = http.Get(fmt.Sprintf("http://%s/log-level -X PUT -d fatal", debugAddr))
-		Ω(err).ShouldNot(HaveOccurred())
+			_, err = http.Get(fmt.Sprintf("http://%s/log-level -X PUT -d fatal", debugAddr))
+			Ω(err).ShouldNot(HaveOccurred())
+		})
 	})
-
 })
