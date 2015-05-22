c84ef7cbec574fb02eb3707aed67f79deec2b61a
Glyn Normington <gnormington@pivotal.io>
2015-05-22 16:58:39 +0100

    Build programs in suite.
    Building in tests is slow and race-prone.
    
    [#90094414]

--- container_daemon/proc_starter_linux_test.go
+++ container_daemon/proc_starter_linux_test.go
@@ -6,24 +6,14 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("proc_starter", func() {
-
-	var procStarter string
-
-	BeforeEach(func() {
-		var err error
-		procStarter, err = gexec.Build("github.com/cloudfoundry-incubator/garden-linux/container_daemon/proc_starter")
-		Expect(err).ToNot(HaveOccurred())
-	})
-
 	It("runs the process in the specified working directory", func() {
 		testWorkDir, err := ioutil.TempDir("", "")
 		Expect(err).ToNot(HaveOccurred())
 
-		cmd := exec.Command(procStarter, "/bin/sh", "-c", "echo $PWD")
+		cmd := exec.Command(procStarterBin, "/bin/sh", "-c", "echo $PWD")
 		cmd.Dir = testWorkDir
 		op, err := cmd.CombinedOutput()
 		Expect(err).ToNot(HaveOccurred())
--- container_daemon/wsh_integration_linux_test.go
+++ container_daemon/wsh_integration_linux_test.go
@@ -16,7 +16,6 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
-	"github.com/onsi/gomega/gexec"
 )
 
 type FakeCommandRunner struct {
@@ -34,16 +33,9 @@ var _ = Describe("wsh and daemon integration", func() {
 	var daemon *container_daemon.ContainerDaemon
 	var tempDir string
 	var socketPath string
-	var wsh string
 
 	BeforeEach(func() {
 		var err error
-		wsh, err = gexec.Build("github.com/cloudfoundry-incubator/garden-linux/container_daemon/wsh")
-		Expect(err).ToNot(HaveOccurred())
-
-		proc_starter, err := gexec.Build("github.com/cloudfoundry-incubator/garden-linux/container_daemon/proc_starter")
-		Expect(err).ToNot(HaveOccurred())
-
 		tempDir, err = ioutil.TempDir("", "")
 		Expect(err).ToNot(HaveOccurred())
 		socketPath = path.Join(tempDir, "test.sock")
@@ -54,7 +46,7 @@ var _ = Describe("wsh and daemon integration", func() {
 			},
 			CmdPreparer: &container_daemon.ProcessSpecPreparer{
 				Users:           system.LibContainerUser{},
-				ProcStarterPath: proc_starter,
+				ProcStarterPath: procStarterBin,
 				Rlimits:         &container_daemon.RlimitsManager{},
 			},
 			Spawner: &container_daemon.Spawn{
@@ -71,7 +63,7 @@ var _ = Describe("wsh and daemon integration", func() {
 	})
 
 	It("should run a program when no pidfile is specified", func() {
-		wshCmd := exec.Command(wsh,
+		wshCmd := exec.Command(wshBin,
 			"--socket", socketPath,
 			"--user", "root",
 			"echo", "hello")
@@ -84,7 +76,7 @@ var _ = Describe("wsh and daemon integration", func() {
 	It("should avoid a race condition when sending a kill signal", func(done Done) {
 		for i := 0; i < 200; i++ {
 			pidfilePath := path.Join(tempDir, "cmd.pid")
-			wshCmd := exec.Command(wsh,
+			wshCmd := exec.Command(wshBin,
 				"--socket", socketPath,
 				"--pidfile", pidfilePath,
 				"--user", "root",
@@ -105,7 +97,7 @@ var _ = Describe("wsh and daemon integration", func() {
 		stdout := gbytes.NewBuffer()
 
 		pidfilePath := path.Join(tempDir, "cmd.pid")
-		wshCmd := exec.Command(wsh,
+		wshCmd := exec.Command(wshBin,
 			"--socket", socketPath,
 			"--pidfile", pidfilePath,
 			"--user", "root",
@@ -137,7 +129,7 @@ var _ = Describe("wsh and daemon integration", func() {
 		for i := 0; i < 200; i++ {
 			stdout := gbytes.NewBuffer()
 
-			wshCmd := exec.Command(wsh,
+			wshCmd := exec.Command(wshBin,
 				"--socket", socketPath,
 				"--user", "root",
 				"sh", "-c", `
@@ -161,7 +153,7 @@ var _ = Describe("wsh and daemon integration", func() {
 	}, 30.0)
 
 	It("applies the provided rlimits", func() {
-		wshCmd := exec.Command(wsh,
+		wshCmd := exec.Command(wshBin,
 			"--socket", socketPath,
 			"--user", "root",
 			"sh", "-c",
