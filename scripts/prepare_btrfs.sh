470c619bed42c2eaed91260b1ad5f109bb9c28e7
Craig Furman <cfurman@pivotal.io>
2015-06-05 12:15:19 +0100

    Install btrfs tools in CI images
    prepare_btrfs mounts a btrfs volume for non-nested testing
    
    Signed-off-by: Julian Friedman <julz.friedman@uk.ibm.com>

--- scripts/prepare_btrfs.sh
+++ scripts/prepare_btrfs.sh
@@ -3,47 +3,29 @@
 set -e
 set -x
 
-function prepareBtrfs()
-{
-    apt-get update
-    apt-get install -y asciidoc xmlto --no-install-recommends
-    apt-get install -y pkg-config autoconf
-    apt-get build-dep -y btrfs-tools
-    mkdir -p $HOME/btrfs
-    pushd $HOME/btrfs
-        git clone git://git.kernel.org/pub/scm/linux/kernel/git/kdave/btrfs-progs.git
-        cd btrfs-progs
-        ./autogen.sh
-        ./configure
-        make
-        make install
-    popd
-}
-
 if [ $(whoami) != "root" ]
 then
     echo "Must be run as root" >&2
     exit 1
 fi
 
-if [ ! -f /usr/include/btrfs/version.h ]
-then
-    prepareBtrfs
-else
-    echo "btrfs tools already installed"
-fi
-
 backing_store=/opt/btrfs_backing_store
 loopback_device=/dev/btrfs_loop
 mount_point=/tmp/btrfs_mount
-if [ ! -f $backing_store ]
+
+if [ ! -d $mount_point ]
 then
     dd if=/dev/zero of=$backing_store bs=1M count=3000
     mknod $loopback_device b 7 200
     losetup $loopback_device $backing_store
     mkfs.btrfs $backing_store
+fi
+
+if [ -z $(df $mount_point) ]
+then
+    echo "mounting btrfs volume"
     mkdir -p $mount_point
     mount -t btrfs $loopback_device $mount_point
 else
-    echo "btrfs mount already set up, skipping"
+    echo "btrfs volume already mounted"
 fi
