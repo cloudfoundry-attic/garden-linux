4d560207e78c3f7a931cd814621acd81e586b7d3
George Lestaris <glestaris@pivotal.io>
2015-06-11 17:18:19 +0100

    Add test for limit disk behavior on rootfses with pre-existing users
    * Refactoring of the LimitDisk lifecycle test
    * Adds cloudfoundry/preexisting_users to garden-ci
    
    [finishes #95436952]

--- integration/lifecycle/limits_test.go
+++ integration/lifecycle/limits_test.go
@@ -60,165 +60,163 @@ var _ = Describe("Limits", func() {
 		})
 	})
 
-	Context("without a disk limit (quota disabled)", func() {
-		BeforeEach(func() {
-			startGardenArgs = []string{"-disableQuotas=true"}
-			rootfs = rootFSPath
-			privilegedContainer = true
-		})
-
-		Context("when there is a disk quota", func() {
-			quotaLimit := garden.DiskLimits{
-				ByteSoft: 5 * 1024 * 1024,
-				ByteHard: 5 * 1024 * 1024,
-			}
-
-			JustBeforeEach(func() {
-				Expect(container.LimitDisk(quotaLimit)).To(Succeed())
-			})
-
-			It("reports the disk limit size of the container as zero", func() {
-				limit, err := container.CurrentDiskLimits()
-				Expect(err).ToNot(HaveOccurred())
-				Expect(limit).To(Equal(garden.DiskLimits{}))
-			})
-
-			Context("and run a process that exceeds the quota", func() {
-				It("does not kill the process", func() {
-					dd, err := container.Run(garden.ProcessSpec{
-						User: "vcap",
-						Path: "dd",
-						Args: []string{"if=/dev/zero", "of=/tmp/some-file", "bs=1M", "count=10"},
-					}, garden.ProcessIO{})
-					Expect(err).ToNot(HaveOccurred())
-					Expect(dd.Wait()).To(Equal(0))
-				})
-			})
-		})
-	})
-
-	Context("with a disk limit", func() {
-		if os.Getenv("BTRFS_SUPPORTED") == "" {
-			log.Println("btrfs not supported: skipping disk limit tests")
-			return
-		}
-
-		BeforeEach(func() {
-			privilegedContainer = false
-			rootfs = rootFSPath
-		})
-
-		JustBeforeEach(func() {
-			err := container.LimitDisk(garden.DiskLimits{
-				ByteSoft: 180 * 1024 * 1024,
-				ByteHard: 180 * 1024 * 1024,
-			})
-
-			Expect(err).ToNot(HaveOccurred())
-		})
-
-		It("reports correct disk usage", func() {
-			var diskUsage uint64
-			stdout := gbytes.NewBuffer()
-
-			process, err := container.Run(garden.ProcessSpec{
-				User: "vcap",
-				Path: "sh",
-				Args: []string{"-c", "du -d 0 / | awk ' {print $1 }'"},
-			}, garden.ProcessIO{Stdout: stdout})
-			Expect(err).ToNot(HaveOccurred())
-			Expect(process.Wait()).To(Equal(0))
-
-			_, err = fmt.Sscanf(strings.TrimSpace(string(stdout.Contents())), "%d", &diskUsage)
-			Expect(err).ToNot(HaveOccurred())
-
-			process, err = container.Run(garden.ProcessSpec{
-				User: "vcap",
-				Path: "dd",
-				Args: []string{"if=/dev/urandom", "of=/home/vcap/some-file", "bs=1M", "count=80"},
-			}, garden.ProcessIO{})
-			Expect(err).ToNot(HaveOccurred())
-			Expect(process.Wait()).To(Equal(0))
-
-			metrics := func() uint64 {
-				metricsAfter, err := container.Metrics()
-				Expect(err).ToNot(HaveOccurred())
-
-				return metricsAfter.DiskStat.BytesUsed
-			}
-
-			expectedBytes := (diskUsage * 1024) + uint64(80*1024*1024)
-			Eventually(metrics, 90, 30).Should(BeNumerically("~", expectedBytes, 1269760))
-
-			process, err = container.Run(garden.ProcessSpec{
-				User: "vcap",
-				Path: "dd",
-				Args: []string{"if=/dev/urandom", "of=/home/vcap/another-file", "bs=1M", "count=80"},
-			}, garden.ProcessIO{})
-			Expect(err).ToNot(HaveOccurred())
-			Expect(process.Wait()).To(Equal(0))
-
-			expectedBytes = (diskUsage * 1024) + uint64(80*1024*1024) + uint64(80*1024*1024)
-			Eventually(metrics, 90, 30).Should(BeNumerically("~", expectedBytes, 1269760))
-		})
-
-		Context("when multiple containers are created for the same user", func() {
-			var container2 garden.Container
-			var err error
-
-			JustBeforeEach(func() {
-				container2, err = client.Create(garden.ContainerSpec{
-					Privileged: privilegedContainer,
-					RootFSPath: rootfs,
-				})
-				Expect(err).ToNot(HaveOccurred())
-
-				err = container.LimitDisk(garden.DiskLimits{
-					ByteSoft: 50 * 1024 * 1024,
-					ByteHard: 50 * 1024 * 1024,
-				})
-				Expect(err).ToNot(HaveOccurred())
-			})
-
-			AfterEach(func() {
-				if container2 != nil {
-					Expect(client.Destroy(container2.Handle())).To(Succeed())
-				}
-			})
-
-			It("gives each container its own quota", func() {
-				process, err := container.Run(garden.ProcessSpec{
-					User: "vcap",
-					Path: "dd",
-					Args: []string{"if=/dev/urandom", "of=/home/vcap/some-file", "bs=1M", "count=40"},
-				}, garden.ProcessIO{})
-				Expect(err).ToNot(HaveOccurred())
-				Expect(process.Wait()).To(Equal(0))
-
-				process, err = container2.Run(garden.ProcessSpec{
-					User: "vcap",
-					Path: "dd",
-					Args: []string{"if=/dev/urandom", "of=/home/vcap/some-file", "bs=1M", "count=40"},
-				}, garden.ProcessIO{})
-				Expect(err).ToNot(HaveOccurred())
-				Expect(process.Wait()).To(Equal(0))
-			})
-		})
-
-		Context("on an unprivileged Docker container", func() {
+	Describe("LimitDisk", func() {
+		Context("when quotas are disabled", func() {
 			BeforeEach(func() {
-				privilegedContainer = false
-				rootfs = "docker:///busybox"
+				startGardenArgs = []string{"-disableQuotas=true"}
+				rootfs = rootFSPath
+				privilegedContainer = true
 			})
 
-			Context("when there is a disk quota", func() {
+			Context("and there is a disk limit", func() {
 				quotaLimit := garden.DiskLimits{
-					ByteSoft: 10 * 1024 * 1024,
-					ByteHard: 10 * 1024 * 1024,
+					ByteSoft: 5 * 1024 * 1024,
+					ByteHard: 5 * 1024 * 1024,
 				}
 
 				JustBeforeEach(func() {
 					Expect(container.LimitDisk(quotaLimit)).To(Succeed())
+				})
+
+				It("reports the disk limit size of the container as zero", func() {
+					limit, err := container.CurrentDiskLimits()
+					Expect(err).ToNot(HaveOccurred())
+					Expect(limit).To(Equal(garden.DiskLimits{}))
+				})
+
+				Context("and it runs a process that exceeds the limit", func() {
+					It("does not kill the process", func() {
+						dd, err := container.Run(garden.ProcessSpec{
+							User: "vcap",
+							Path: "dd",
+							Args: []string{"if=/dev/zero", "of=/tmp/some-file", "bs=1M", "count=6"},
+						}, garden.ProcessIO{})
+						Expect(err).ToNot(HaveOccurred())
+						Expect(dd.Wait()).To(Equal(0))
+					})
+				})
+			})
+		})
+
+		Context("when quotas are enabled and there is a disk limit", func() {
+			var quotaLimit garden.DiskLimits
+
+			if os.Getenv("BTRFS_SUPPORTED") == "" {
+				log.Println("btrfs not supported: skipping disk limit tests")
+				return
+			}
+
+			BeforeEach(func() {
+				quotaLimit = garden.DiskLimits{
+					ByteSoft: 180 * 1024 * 1024,
+					ByteHard: 180 * 1024 * 1024,
+				}
+			})
+
+			JustBeforeEach(func() {
+				err := container.LimitDisk(quotaLimit)
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			Context("on a directory rootfs container", func() {
+				BeforeEach(func() {
+					privilegedContainer = false
+					rootfs = rootFSPath
+				})
+
+				It("reports correct disk usage", func() {
+					var diskUsage uint64
+					stdout := gbytes.NewBuffer()
+
+					process, err := container.Run(garden.ProcessSpec{
+						User: "vcap",
+						Path: "sh",
+						Args: []string{"-c", "du -d 0 / | awk ' {print $1 }'"},
+					}, garden.ProcessIO{Stdout: stdout})
+					Expect(err).ToNot(HaveOccurred())
+					Expect(process.Wait()).To(Equal(0))
+
+					_, err = fmt.Sscanf(strings.TrimSpace(string(stdout.Contents())), "%d", &diskUsage)
+					Expect(err).ToNot(HaveOccurred())
+
+					process, err = container.Run(garden.ProcessSpec{
+						User: "vcap",
+						Path: "dd",
+						Args: []string{"if=/dev/urandom", "of=/home/vcap/some-file", "bs=1M", "count=10"},
+					}, garden.ProcessIO{})
+					Expect(err).ToNot(HaveOccurred())
+					Expect(process.Wait()).To(Equal(0))
+
+					metrics := func() uint64 {
+						metricsAfter, err := container.Metrics()
+						Expect(err).ToNot(HaveOccurred())
+
+						return metricsAfter.DiskStat.BytesUsed
+					}
+
+					expectedBytes := (diskUsage * 1024) + uint64(10*1024*1024)
+					Eventually(metrics, 90, 30).Should(BeNumerically("~", expectedBytes, 1269760))
+
+					process, err = container.Run(garden.ProcessSpec{
+						User: "vcap",
+						Path: "dd",
+						Args: []string{"if=/dev/urandom", "of=/home/vcap/another-file", "bs=1M", "count=10"},
+					}, garden.ProcessIO{})
+					Expect(err).ToNot(HaveOccurred())
+					Expect(process.Wait()).To(Equal(0))
+
+					expectedBytes = (diskUsage * 1024) + uint64(20*1024*1024)
+					Eventually(metrics, 90, 30).Should(BeNumerically("~", expectedBytes, 1269760))
+				})
+
+				Context("on a rootfs with pre-existing users", func() {
+					if os.Getenv("GARDEN_PREEXISTING_USERS_TEST_ROOTFS") == "" {
+						log.Println("pre-existing users rootfs not found: skipping some limit disk tests")
+						return
+					}
+
+					BeforeEach(func() {
+						rootFSPath = os.Getenv("GARDEN_PREEXISTING_USERS_TEST_ROOTFS")
+						quotaLimit = garden.DiskLimits{
+							ByteSoft: 10 * 1024 * 1024,
+							ByteHard: 10 * 1024 * 1024,
+						}
+					})
+
+					Context("and run a process that exceeds the quota as bob", func() {
+						It("kills the process", func() {
+							dd, err := container.Run(garden.ProcessSpec{
+								User: "bob",
+								Path: "dd",
+								Args: []string{"if=/dev/zero", "of=/home/bob/test", "count=22528"},
+							}, garden.ProcessIO{})
+							Expect(err).ToNot(HaveOccurred())
+							Expect(dd.Wait()).ToNot(Equal(0))
+						})
+					})
+
+					Context("and run a process that exceeds the quota as alice", func() {
+						It("kills the process", func() {
+							dd, err := container.Run(garden.ProcessSpec{
+								User: "alice",
+								Path: "dd",
+								Args: []string{"if=/dev/zero", "of=/home/alice/test", "bs=1M", "count=11"},
+							}, garden.ProcessIO{})
+							Expect(err).ToNot(HaveOccurred())
+							Expect(dd.Wait()).ToNot(Equal(0))
+						})
+					})
+				})
+			})
+
+			Context("on a Docker container", func() {
+				BeforeEach(func() {
+					privilegedContainer = false
+					rootfs = "docker:///busybox"
+					quotaLimit = garden.DiskLimits{
+						ByteSoft: 10 * 1024 * 1024,
+						ByteHard: 10 * 1024 * 1024,
+					}
 				})
 
 				It("reports the correct disk limit size of the container", func() {
@@ -232,67 +230,129 @@ var _ = Describe("Limits", func() {
 						dd, err := container.Run(garden.ProcessSpec{
 							User: "vcap",
 							Path: "dd",
-							Args: []string{"if=/dev/zero", "of=/root/test", "count=152400"},
+							Args: []string{"if=/dev/zero", "of=/root/test", "bs=1M", "count=11"},
 						}, garden.ProcessIO{})
 						Expect(err).ToNot(HaveOccurred())
 						Expect(dd.Wait()).ToNot(Equal(0))
 					})
 				})
-			})
-		})
 
-		Context("on a privileged Docker container", func() {
-			BeforeEach(func() {
-				privilegedContainer = true
-				rootfs = "docker:///busybox"
+				Context("on a rootfs with pre-existing users", func() {
+					BeforeEach(func() {
+						rootFSPath = "docker:///cloudfoundry/preexisting_users"
+						quotaLimit = garden.DiskLimits{
+							ByteSoft: 10 * 1024 * 1024,
+							ByteHard: 10 * 1024 * 1024,
+						}
+					})
+
+					Context("and run a process that exceeds the quota as bob", func() {
+						It("kills the process", func() {
+							dd, err := container.Run(garden.ProcessSpec{
+								User: "bob",
+								Path: "dd",
+								Args: []string{"if=/dev/zero", "of=/home/bob/test", "count=22528"},
+							}, garden.ProcessIO{})
+							Expect(err).ToNot(HaveOccurred())
+							Expect(dd.Wait()).ToNot(Equal(0))
+						})
+					})
+
+					Context("and run a process that exceeds the quota as alice", func() {
+						It("kills the process", func() {
+							dd, err := container.Run(garden.ProcessSpec{
+								User: "alice",
+								Path: "dd",
+								Args: []string{"if=/dev/zero", "of=/home/alice/test", "bs=1M", "count=11"},
+							}, garden.ProcessIO{})
+							Expect(err).ToNot(HaveOccurred())
+							Expect(dd.Wait()).ToNot(Equal(0))
+						})
+					})
+				})
+
+				Context("that is privileged", func() {
+					BeforeEach(func() {
+						privilegedContainer = true
+					})
+
+					Context("and run a process that exceeds the quota as root", func() {
+						It("kills the process", func() {
+							dd, err := container.Run(garden.ProcessSpec{
+								User: "root",
+								Path: "dd",
+								Args: []string{"if=/dev/zero", "of=/root/test", "bs=1M", "count=11"},
+							}, garden.ProcessIO{})
+							Expect(err).ToNot(HaveOccurred())
+							Expect(dd.Wait()).ToNot(Equal(0))
+						})
+					})
+
+					Context("and run a process that exceeds the quota as a new user", func() {
+						It("kills the process", func() {
+							addUser, err := container.Run(garden.ProcessSpec{
+								User: "root",
+								Path: "adduser",
+								Args: []string{"-D", "-g", "", "bob"},
+							}, garden.ProcessIO{})
+							Expect(err).ToNot(HaveOccurred())
+							Expect(addUser.Wait()).To(Equal(0))
+
+							dd, err := container.Run(garden.ProcessSpec{
+								User: "bob",
+								Path: "dd",
+								Args: []string{"if=/dev/zero", "of=/home/bob/test", "bs=1M", "count=11"},
+							}, garden.ProcessIO{})
+							Expect(err).ToNot(HaveOccurred())
+							Expect(dd.Wait()).ToNot(Equal(0))
+						})
+					})
+				})
 			})
 
-			Context("when there is a disk quota", func() {
-				quotaLimit := garden.DiskLimits{
-					ByteSoft: 10 * 1024 * 1024,
-					ByteHard: 10 * 1024 * 1024,
-				}
+			Context("when multiple containers are created for the same user", func() {
+				var container2 garden.Container
+				var err error
+
+				BeforeEach(func() {
+					privilegedContainer = false
+					rootfs = rootFSPath
+					quotaLimit = garden.DiskLimits{
+						ByteSoft: 50 * 1024 * 1024,
+						ByteHard: 50 * 1024 * 1024,
+					}
+				})
 
 				JustBeforeEach(func() {
-					Expect(container.LimitDisk(quotaLimit)).To(Succeed())
-				})
-
-				It("reports the correct disk limit size of the container", func() {
-					limit, err := container.CurrentDiskLimits()
+					container2, err = client.Create(garden.ContainerSpec{
+						Privileged: privilegedContainer,
+						RootFSPath: rootfs,
+					})
 					Expect(err).ToNot(HaveOccurred())
-					Expect(limit).To(Equal(quotaLimit))
 				})
 
-				Context("and run a process that exceeds the quota as root", func() {
-					It("kills the process", func() {
-						dd, err := container.Run(garden.ProcessSpec{
-							User: "root",
-							Path: "dd",
-							Args: []string{"if=/dev/zero", "of=/root/test", "count=152400"},
-						}, garden.ProcessIO{})
-						Expect(err).ToNot(HaveOccurred())
-						Expect(dd.Wait()).ToNot(Equal(0))
-					})
+				AfterEach(func() {
+					if container2 != nil {
+						Expect(client.Destroy(container2.Handle())).To(Succeed())
+					}
 				})
 
-				Context("and run a process that exceeds the quota as a new user", func() {
-					It("kills the process", func() {
-						addUser, err := container.Run(garden.ProcessSpec{
-							User: "root",
-							Path: "adduser",
-							Args: []string{"-D", "-g", "", "bob"},
-						}, garden.ProcessIO{})
-						Expect(err).ToNot(HaveOccurred())
-						Expect(addUser.Wait()).To(Equal(0))
+				It("gives each container its own quota", func() {
+					process, err := container.Run(garden.ProcessSpec{
+						User: "vcap",
+						Path: "dd",
+						Args: []string{"if=/dev/urandom", "of=/home/vcap/some-file", "bs=1M", "count=40"},
+					}, garden.ProcessIO{})
+					Expect(err).ToNot(HaveOccurred())
+					Expect(process.Wait()).To(Equal(0))
 
-						dd, err := container.Run(garden.ProcessSpec{
-							User: "bob",
-							Path: "dd",
-							Args: []string{"if=/dev/zero", "of=/home/bob/test", "count=152400"},
-						}, garden.ProcessIO{})
-						Expect(err).ToNot(HaveOccurred())
-						Expect(dd.Wait()).ToNot(Equal(0))
-					})
+					process, err = container2.Run(garden.ProcessSpec{
+						User: "vcap",
+						Path: "dd",
+						Args: []string{"if=/dev/urandom", "of=/home/vcap/some-file", "bs=1M", "count=40"},
+					}, garden.ProcessIO{})
+					Expect(err).ToNot(HaveOccurred())
+					Expect(process.Wait()).To(Equal(0))
 				})
 			})
 		})
