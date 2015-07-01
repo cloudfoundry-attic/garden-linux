3f86f02bcfd465c6977810560c3d595c8960099b
Steffen Uhlig <Steffen.Uhlig@gmail.com>
2015-07-01 12:02:15 +0100

    build capability tool with CGO_ENABLED=0 and static link compiling
    Signed-off-by: Svett Ralchev <sralchev@pivotal.io>

--- integration/lifecycle/lifecycle_suite_test.go
+++ integration/lifecycle/lifecycle_suite_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/onsi/gomega/gexec"
 
 	"github.com/cloudfoundry-incubator/garden-linux/integration/runner"
+	"github.com/golang/go/src/pkg/path"
 )
 
 var (
@@ -44,8 +45,17 @@ func TestLifecycle(t *testing.T) {
 		shmPath, err := gexec.Build("github.com/cloudfoundry-incubator/garden-linux/integration/lifecycle/shm_test")
 		Expect(err).ToNot(HaveOccurred())
 
-		capabilityPath, err := gexec.Build("github.com/cloudfoundry-incubator/garden-linux/integration/helpers/capability")
+		os.Setenv("CGO_ENABLED", "0")
+		capabilityPath, err := gexec.Build("github.com/cloudfoundry-incubator/garden-linux/integration/helpers/capability", "-a", "-installsuffix", "static")
 		Expect(err).ToNot(HaveOccurred())
+		os.Unsetenv("CGO_ENABLED")
+
+		os.Chmod(capabilityPath, 777)
+		os.Chown(capabilityPath, 0, 0)
+
+		capabilityDir := path.Dir(capabilityPath)
+		os.Chmod(capabilityDir, 777)
+		os.Chown(capabilityDir, 0, 0)
 
 		data := fmt.Sprintf("%s|%s", shmPath, capabilityPath)
 		return []byte(data)
