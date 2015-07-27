d01dff5e4249db464f55d5a43d273d706c5b98c8
Marc Schunk <Marc.Schunk@de.ibm.com>
2015-07-27 17:34:16 +0100

    Revert "Remove NamespacedSignaller as it is not used anymore"
    We need this for backward compatibility
    
    This reverts commit 702fb19c782ca66271f10bb0a0528fb1007c29c5.

---
+++ linux_backend/namespaced_signaller.go
@@ -0,0 +1,105 @@
+package linux_backend
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"time"
+
+	"github.com/cloudfoundry/gunk/command_runner"
+	"github.com/pivotal-golang/lager"
+)
+
+// Kills a process by invoking ./bin/wsh in the given container path using
+// a PID read from the given pidFile
+type NamespacedSignaller struct {
+	Runner        command_runner.CommandRunner
+	ContainerPath string
+	PidFilePath   string
+	Logger        lager.Logger
+}
+
+func (n *NamespacedSignaller) Signal(signal os.Signal) error {
+	n.Logger.Debug("NamespacedSignaller.Signal-entered", lager.Data{"signal": signal})
+	pid, err := PidFromFile(n.PidFilePath)
+	if err != nil {
+		n.Logger.Error("NamespacedSignaller.Signal-failed-to-read-PID-file", err, lager.Data{"signal": signal})
+		return err
+	}
+
+	cmd := exec.Command(filepath.Join(n.ContainerPath, "bin/wsh"),
+		"--socket", filepath.Join(n.ContainerPath, "run/wshd.sock"),
+		"--user", "root",
+		"kill", fmt.Sprintf("-%d", signal), fmt.Sprintf("%d", pid))
+
+	n.Logger.Debug("NamespacedSignaller.Signal-about-to-run-kill-command", lager.Data{"signal": signal, "cmd": cmd})
+	err = n.Runner.Run(cmd)
+	if err != nil {
+		n.Logger.Error("NamespacedSignaller.Signal-failed-to-run-kill", err, lager.Data{"signal": signal})
+		return err
+	}
+
+	n.Logger.Debug("NamespacedSignaller.Signal-ran-kill-successfully", lager.Data{"signal": signal})
+	return nil
+}
+
+func PidFromFile(pidFilePath string) (int, error) {
+	pidFile, err := openPIDFile(pidFilePath)
+	if err != nil {
+		return 0, err
+	}
+	defer pidFile.Close()
+
+	fileContent, err := readPIDFile(pidFile)
+	if err != nil {
+		return 0, err
+	}
+
+	var pid int
+	_, err = fmt.Sscanf(fileContent, "%d", &pid)
+	if err != nil {
+		return 0, fmt.Errorf("linux_backend: can't parse PID file content: %v", err)
+	}
+
+	return pid, nil
+}
+
+func openPIDFile(pidFilePath string) (*os.File, error) {
+	var err error
+
+	for i := 0; i < 100; i++ { // 10 seconds
+		var pidFile *os.File
+		pidFile, err = os.Open(pidFilePath)
+		if err == nil {
+			return pidFile, nil
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
+
+	return nil, fmt.Errorf("linux_backend: can't open PID file: %s", err)
+}
+
+func readPIDFile(pidFile *os.File) (string, error) {
+	var bytesReadAmt int
+
+	buffer := make([]byte, 32)
+	for i := 0; i < 100; i++ { // retry 10 seconds
+		bytesReadAmt, _ = pidFile.Read(buffer)
+
+		if bytesReadAmt == 0 {
+			pidFile.Seek(0, 0)
+			time.Sleep(time.Millisecond * 100)
+			continue
+		}
+		break
+	}
+
+	if bytesReadAmt == 0 {
+		return "", errors.New("linux_backend: can't read PID file: is empty or non existent")
+	}
+
+	return string(buffer), nil
+}
---
+++ linux_backend/namespaced_signaller_test.go
@@ -0,0 +1,98 @@
+package linux_backend_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/pivotal-golang/lager/lagertest"
+
+	"github.com/cloudfoundry-incubator/garden-linux/linux_backend"
+	"github.com/cloudfoundry/gunk/command_runner/fake_command_runner"
+	. "github.com/cloudfoundry/gunk/command_runner/fake_command_runner/matchers"
+)
+
+var _ = Describe("Namespaced Signaller", func() {
+	It("kills a process using ./bin/wsh based on its pid", func() {
+		tmp, err := ioutil.TempDir("", "namespacedsignaller")
+		Expect(err).ToNot(HaveOccurred())
+		defer os.RemoveAll(tmp)
+
+		pidFile := filepath.Join(tmp, "thepid.file")
+
+		fakeRunner := fake_command_runner.New()
+		signaller := &linux_backend.NamespacedSignaller{
+			Runner:        fakeRunner,
+			ContainerPath: "/fish/finger",
+			PidFilePath:   pidFile,
+			Logger:        lagertest.NewTestLogger("test"),
+		}
+
+		Expect(ioutil.WriteFile(pidFile, []byte(" 12345\n"), 0755)).To(Succeed())
+
+		Expect(signaller.Signal(os.Kill)).To(Succeed())
+		Expect(fakeRunner).To(HaveExecutedSerially(
+			fake_command_runner.CommandSpec{
+				Path: "/fish/finger/bin/wsh",
+				Args: []string{
+					"--socket", "/fish/finger/run/wshd.sock",
+					"--user", "root",
+					"kill", "-9", "12345",
+				},
+			}))
+	})
+
+	It("returns an appropriate error when the pidfile is not present", func() {
+		fakeRunner := fake_command_runner.New()
+		signaller := &linux_backend.NamespacedSignaller{
+			Runner:        fakeRunner,
+			ContainerPath: "/fish/finger",
+			PidFilePath:   "/does/not/exist",
+			Logger:        lagertest.NewTestLogger("test"),
+		}
+
+		Expect(signaller.Signal(os.Kill)).To(MatchError("linux_backend: can't open PID file: open /does/not/exist: no such file or directory"))
+	})
+
+	It("returns an appropriate error when the pidfile is empty", func() {
+		tmp, err := ioutil.TempDir("", "namespacedsignaller")
+		Expect(err).ToNot(HaveOccurred())
+		defer os.RemoveAll(tmp)
+
+		pidFile := filepath.Join(tmp, "thepid.file")
+
+		fakeRunner := fake_command_runner.New()
+		signaller := &linux_backend.NamespacedSignaller{
+			Runner:        fakeRunner,
+			ContainerPath: "/fish/finger",
+			PidFilePath:   pidFile,
+			Logger:        lagertest.NewTestLogger("test"),
+		}
+
+		Expect(ioutil.WriteFile(pidFile, []byte(""), 0755)).To(Succeed())
+
+		Expect(signaller.Signal(os.Kill)).To(MatchError("linux_backend: can't read PID file: is empty or non existent"))
+	})
+
+	It("returns an appropriate error when the pidfile does not contain a number", func() {
+		tmp, err := ioutil.TempDir("", "namespacedsignaller")
+		Expect(err).ToNot(HaveOccurred())
+		defer os.RemoveAll(tmp)
+
+		pidFile := filepath.Join(tmp, "thepid.file")
+
+		fakeRunner := fake_command_runner.New()
+		signaller := &linux_backend.NamespacedSignaller{
+			Runner:        fakeRunner,
+			ContainerPath: "/fish/finger",
+			PidFilePath:   pidFile,
+			Logger:        lagertest.NewTestLogger("test"),
+		}
+
+		Expect(ioutil.WriteFile(pidFile, []byte("not-a-pid\n"), 0755)).To(Succeed())
+
+		Expect(signaller.Signal(os.Kill)).To(MatchError("linux_backend: can't parse PID file content: expected integer"))
+	})
+})
