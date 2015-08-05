a5ef790ca2d9ea916de7e0884415c424a64a92d6
Julian Friedman <julz.friedman@uk.ibm.com>
2015-08-05 10:15:27 +0100

    Delete qgroup when subvolume is deleted

--- rootfs_provider/btrfs_cleanup/btrfs_cleanup.go
+++ rootfs_provider/btrfs_cleanup/btrfs_cleanup.go
@@ -2,11 +2,13 @@ package btrfs_cleanup
 
 import (
 	"bytes"
+	"fmt"
 	"os/exec"
 	"path/filepath"
 	"sort"
 	"strings"
 
+	"github.com/cloudfoundry-incubator/garden-linux/logging"
 	"github.com/cloudfoundry-incubator/garden-linux/rootfs_provider"
 	"github.com/cloudfoundry/gunk/command_runner"
 	"github.com/pivotal-golang/lager"
@@ -17,25 +19,65 @@ type BtrfsRootFSRemover struct {
 	GraphDriver     rootfs_provider.GraphDriver
 	BtrfsMountPoint string
 	RemoveAll       func(dir string) error
+
+	Logger lager.Logger
 }
 
 func (c *BtrfsRootFSRemover) CleanupRootFS(logger lager.Logger, id string) error {
 	c.GraphDriver.Put(id)
 
-	if err := c.clean(id); err != nil {
+	log := c.Logger.Session("clean-rootfs", lager.Data{"id": id})
+	log.Info("start")
+
+	layerPath, err := c.GraphDriver.Get(id, "")
+	if err != nil {
+		log.Error("get", err)
 		return err
 	}
 
+	if err := c.removeSubvols(log, layerPath); err != nil {
+		return err
+	}
+
+	if err := c.removeQgroup(log, layerPath); err != nil {
+		log.Error("remove-qgroup", err)
+	}
+
+	defer log.Info("complete")
 	return c.GraphDriver.Remove(id)
 }
 
-func (c *BtrfsRootFSRemover) clean(id string) error {
-	layerPath, err := c.GraphDriver.Get(id, "")
+func (c *BtrfsRootFSRemover) removeQgroup(log lager.Logger, layerPath string) error {
+	log = log.Session("remove-qgroup")
+	log.Info("start")
+
+	runner := &logging.Runner{c.Runner, log}
+
+	qgroupInfo, err := c.run(runner, exec.Command("btrfs", "qgroup", "show", "-f", layerPath))
 	if err != nil {
 		return err
 	}
 
-	listSubvolumesOutput, err := c.run(exec.Command("btrfs", "subvolume", "list", c.BtrfsMountPoint))
+	qgroupInfoLines := strings.Split(qgroupInfo, "\n")
+	if len(qgroupInfoLines) != 4 {
+		return fmt.Errorf("unexpected qgroup show output: %s", qgroupInfo)
+	}
+
+	qgroupid := strings.SplitN(qgroupInfoLines[2], " ", 2)[0]
+	_, err = c.run(runner, exec.Command("btrfs", "qgroup", "destroy", qgroupid, c.BtrfsMountPoint))
+
+	if err != nil {
+		log.Error("failed", err)
+	}
+
+	log.Info("destroyed", lager.Data{"qgroupid": qgroupid})
+	return nil
+}
+
+func (c *BtrfsRootFSRemover) removeSubvols(log lager.Logger, layerPath string) error {
+	runner := &logging.Runner{c.Runner, log}
+
+	listSubvolumesOutput, err := c.run(runner, exec.Command("btrfs", "subvolume", "list", c.BtrfsMountPoint))
 	if err != nil {
 		return err
 	}
@@ -48,9 +90,11 @@ func (c *BtrfsRootFSRemover) clean(id string) error {
 
 		if strings.Contains(subvolumeAbsPath, layerPath) && subvolumeAbsPath != layerPath {
 			c.RemoveAll(subvolumeAbsPath)
-			if _, err := c.run(exec.Command("btrfs", "subvolume", "delete", subvolumeAbsPath)); err != nil {
+
+			if _, err := c.run(runner, exec.Command("btrfs", "subvolume", "delete", subvolumeAbsPath)); err != nil {
 				return err
 			}
+
 		}
 	}
 
@@ -71,10 +115,10 @@ func finalColumns(lines []string) []string {
 	return result
 }
 
-func (c *BtrfsRootFSRemover) run(cmd *exec.Cmd) (string, error) {
+func (c *BtrfsRootFSRemover) run(runner command_runner.CommandRunner, cmd *exec.Cmd) (string, error) {
 	var buffer bytes.Buffer
 	cmd.Stdout = &buffer
-	if err := c.Runner.Run(cmd); err != nil {
+	if err := runner.Run(cmd); err != nil {
 		return "", err
 	}
 	return buffer.String(), nil
