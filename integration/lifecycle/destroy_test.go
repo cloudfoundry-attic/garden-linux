8fa14020fa9bb35a72fb702812db5f10316bf445
George Lestaris <glestaris@pivotal.io>
2015-10-31 15:25:05 +0000

    Add integration test for AUFS leaks
    Signed-off-by: Julian Friedman <julz.friedman@uk.ibm.com>

--- integration/lifecycle/destroy_test.go
+++ integration/lifecycle/destroy_test.go
@@ -1,14 +1,19 @@
 package lifecycle_test
 
 import (
+	"compress/gzip"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
+	"strings"
 
 	"github.com/cloudfoundry-incubator/garden"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 )
 
 var _ = Describe("Destroying a container", func() {
@@ -44,46 +49,93 @@ var _ = Describe("Destroying a container", func() {
 	})
 
 	Context("when destroying a container with a disk limit", func() {
-		It("should remove the backing store file", func() {
-			client = startGarden()
+		var tgzPath string
 
-			container, err := client.Create(garden.ContainerSpec{
-				RootFSPath: "docker:///busybox",
-				Limits: garden.Limits{
-					Disk: garden.DiskLimits{
-						ByteHard: 10 * 1024 * 1024,
-						Scope:    garden.DiskLimitScopeExclusive,
-					},
-				},
-			})
-			Expect(err).NotTo(HaveOccurred())
-
-			entries, err := ioutil.ReadDir(filepath.Join(client.GraphPath, "backing_stores"))
-			Expect(err).NotTo(HaveOccurred())
-			Expect(entries).To(HaveLen(1))
-
-			Expect(client.Destroy(container.Handle())).To(Succeed())
-
-			entries, err = ioutil.ReadDir(filepath.Join(client.GraphPath, "backing_stores"))
-			Expect(err).NotTo(HaveOccurred())
-			Expect(entries).To(HaveLen(0))
+		BeforeEach(func() {
+			tgzPath = os.Getenv("GARDEN_DORA_PATH")
+			if tgzPath == "" {
+				Skip("`GARDEN_DORA_PATH` is not set")
+			}
 		})
 
 		It("should remove the AUFS directories", func() {
 			client = startGarden()
 
-			container, err := client.Create(garden.ContainerSpec{
-				RootFSPath: "docker:///busybox",
-				Limits: garden.Limits{
-					Disk: garden.DiskLimits{
-						ByteHard: 10 * 1024 * 1024,
-						Scope:    garden.DiskLimitScopeExclusive,
-					},
-				},
-			})
+			var handles []string
+			containersAmt := 5
+
+			h := make(chan string, containersAmt)
+			errs := make(chan error, containersAmt)
+			for i := 0; i < containersAmt; i++ {
+				go func() {
+					defer GinkgoRecover()
+
+					container, err := client.Create(garden.ContainerSpec{
+						RootFSPath: "docker:///busybox",
+						Limits: garden.Limits{
+							Disk: garden.DiskLimits{
+								ByteHard: 100 * 1024 * 1024,
+								Scope:    garden.DiskLimitScopeExclusive,
+							},
+						},
+					})
+					if err != nil {
+						errs <- err
+						return
+					}
+
+					tgz, err := os.Open(tgzPath)
+					if err != nil {
+						errs <- err
+						return
+					}
+
+					tarStream, err := gzip.NewReader(tgz)
+					if err != nil {
+						errs <- err
+						return
+					}
+
+					fmt.Println("stream-in-start")
+					err = container.StreamIn(garden.StreamInSpec{
+						User:      "root",
+						Path:      "/root/dora",
+						TarStream: tarStream,
+					})
+
+					if err != nil {
+						errs <- err
+						return
+					}
+
+					fmt.Println("stream-in-complete")
+
+					h <- container.Handle()
+				}()
+			}
+
+			for i := 0; i < containersAmt; i++ {
+				select {
+				case handle := <-h:
+					handles = append(handles, handle)
+					fmt.Println("created", i)
+				case err := <-errs:
+					Fail(err.Error())
+				}
+			}
+
+			buffer := gbytes.NewBuffer()
+			cmd := exec.Command("sh", "-c", "losetup -a | wc -l")
+			cmd.Stdout = buffer
+			Expect(cmd.Run()).To(Succeed())
+			pdLoopEntLen, err := strconv.ParseInt(strings.TrimSpace(string(buffer.Contents())), 10, 32)
 			Expect(err).NotTo(HaveOccurred())
 
-			entries, err := ioutil.ReadDir(filepath.Join(client.GraphPath, "aufs", "diff"))
+			entries, err := ioutil.ReadDir(filepath.Join(client.GraphPath, "backing_stores"))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(entries).To(HaveLen(containersAmt))
+
+			entries, err = ioutil.ReadDir(filepath.Join(client.GraphPath, "aufs", "diff"))
 			Expect(err).NotTo(HaveOccurred())
 			pdDiffEntLen := len(entries)
 
@@ -91,15 +143,38 @@ var _ = Describe("Destroying a container", func() {
 			Expect(err).NotTo(HaveOccurred())
 			pdMntEntLen := len(entries)
 
-			Expect(client.Destroy(container.Handle())).To(Succeed())
+			errors := make(chan error, containersAmt)
+			for i := 0; i < containersAmt; i++ {
+				go func(i int, ec chan error) {
+					defer GinkgoRecover()
+					errors <- client.Destroy(handles[i])
+				}(i, errors)
+			}
+
+			for i := 0; i < containersAmt; i++ {
+				e := <-errors
+				Expect(e).NotTo(HaveOccurred())
+			}
+
+			buffer = gbytes.NewBuffer()
+			cmd = exec.Command("sh", "-c", "losetup -a | wc -l")
+			cmd.Stdout = buffer
+			Expect(cmd.Run()).To(Succeed())
+			adLoopEntLen, err := strconv.ParseInt(strings.TrimSpace(string(buffer.Contents())), 10, 32)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(adLoopEntLen).To(Equal(pdLoopEntLen - int64(containersAmt)))
+
+			entries, err = ioutil.ReadDir(filepath.Join(client.GraphPath, "backing_stores"))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(entries).To(HaveLen(0))
 
 			entries, err = ioutil.ReadDir(filepath.Join(client.GraphPath, "aufs", "diff"))
 			Expect(err).NotTo(HaveOccurred())
-			Expect(entries).To(HaveLen(pdDiffEntLen - 1))
+			Expect(entries).To(HaveLen(pdDiffEntLen - containersAmt))
 
 			entries, err = ioutil.ReadDir(filepath.Join(client.GraphPath, "aufs", "mnt"))
 			Expect(err).NotTo(HaveOccurred())
-			Expect(entries).To(HaveLen(pdMntEntLen - 1))
+			Expect(entries).To(HaveLen(pdMntEntLen - containersAmt))
 		})
 	})
 })
