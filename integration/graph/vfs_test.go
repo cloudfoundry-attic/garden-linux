55980d17fd1f1eb3796593bd50f32ca01dd36c04
Alex Suraci <suraci.alex@gmail.com>
2016-01-06 12:32:27 -0800

    add support for overlay and vfs drivers
    * add -graphDriver for forcing selection; defaults to "auto"
    * log a warning if anything but aufs is chosen
    * silence docker graph info-level logs

---
+++ integration/graph/vfs_test.go
@@ -0,0 +1,63 @@
+package graph_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+
+	"github.com/cloudfoundry-incubator/garden"
+)
+
+var _ = Describe("VFS", func() {
+	var container garden.Container
+
+	BeforeEach(func() {
+		client = startGarden("-graphDriver", "vfs")
+	})
+
+	It("logs a warning on start", func() {
+		Eventually(client.Buffer).Should(gbytes.Say("unsupported-graph-driver.*vfs"))
+	})
+
+	Describe("a privileged container", func() {
+		JustBeforeEach(func() {
+			var err error
+			container, err = client.Create(garden.ContainerSpec{
+				RootFSPath: rootFSPath,
+				Privileged: true,
+			})
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("can run a process", func() {
+			process, err := container.Run(garden.ProcessSpec{
+				User: "root",
+				Path: "true",
+			}, garden.ProcessIO{Stdout: GinkgoWriter, Stderr: GinkgoWriter})
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(process.Wait()).To(Equal(0))
+		})
+	})
+
+	Describe("an unprivileged container", func() {
+		JustBeforeEach(func() {
+			var err error
+			container, err = client.Create(garden.ContainerSpec{
+				RootFSPath: rootFSPath,
+				Privileged: false,
+			})
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("can run a process", func() {
+			process, err := container.Run(garden.ProcessSpec{
+				User: "root",
+				Path: "true",
+			}, garden.ProcessIO{Stdout: GinkgoWriter, Stderr: GinkgoWriter})
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(process.Wait()).To(Equal(0))
+		})
+	})
+})
