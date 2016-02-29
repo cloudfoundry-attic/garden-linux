2c32db2ee6df2af3695f7750daf0354324efafcf
Julian Friedman <julz.friedman@uk.ibm.com>
2016-02-29 11:23:28 +0000

    Improve implementation of idempotent bridge creation

--- network/devices/bridge.go
+++ network/devices/bridge.go
@@ -19,18 +19,40 @@ func (Bridge) Create(name string, ip net.IP, subnet *net.IPNet) (intf *net.Inter
 	netlinkMu.Lock()
 	defer netlinkMu.Unlock()
 
-	if err := netlink.CreateBridge(name, true); err != nil && err.Error() != "file exists" {
-		return nil, fmt.Errorf("devices: create bridge: %v", err)
-	}
-
-	if intf, err = net.InterfaceByName(name); err != nil {
-		return nil, fmt.Errorf("devices: look up created bridge interface: %v", err)
+	if intf, err = idempotentlyCreateBridge(name); err != nil {
+		return nil, err
 	}
 
 	if err = netlink.NetworkLinkAddIp(intf, ip, subnet); err != nil && err.Error() != "file exists" {
 		return nil, fmt.Errorf("devices: add IP to bridge: %v", err)
 	}
+
 	return intf, nil
+}
+
+func idempotentlyCreateBridge(name string) (intf *net.Interface, err error) {
+	createErr := netlink.CreateBridge(name, true)
+
+	intfs, listErr := net.Interfaces()
+	if listErr != nil {
+		return nil, fmt.Errorf("devices: list bridges: %s", listErr)
+	}
+
+	if intf, ok := findBridgeIntf(intfs, name); ok {
+		return intf, nil
+	}
+
+	return nil, fmt.Errorf("devices: create bridge: ", createErr)
+}
+
+func findBridgeIntf(intfs []net.Interface, name string) (intf *net.Interface, found bool) {
+	for _, intf := range intfs {
+		if intf.Name == name {
+			return &intf, true
+		}
+	}
+
+	return nil, false
 }
 
 func (Bridge) Add(bridge, slave *net.Interface) error {
